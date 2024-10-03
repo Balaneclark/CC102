e4253443ff59be552a640ca97de9383b90fdcdb4
Clarkbalane_10001001 <balaneclark5@gmail.com>
2024-10-03 09:44:19 +0800

    Add files via upload

---
+++ 02Basicoutput.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+	cout << "School\t\t\t:Our Lady of Lourdes College Foundation" << endl;
+	cout << "Department\t\t:College of Computer Studies" << endl;
+	cout << "Course & year level\t:BSIT-1A" << endl;
+	cout << "Fullname\t\t:Clark Marbella Balane" << endl;
+	cout << "Permanent Address\t:P-2b tanauan, capalonga CN" << endl;
+	return 0;
+}
