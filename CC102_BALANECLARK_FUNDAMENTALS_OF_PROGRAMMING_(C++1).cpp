9cbd78c72eac6131c415c98a2e38ad0003ecc5f5
Clarkbalane_10001001 <balaneclark5@gmail.com>
2024-10-15 10:08:34 +0800

    Add files via upload

---
+++ CC102_BALANECLARK_FUNDAMENTALS_OF_PROGRAMMING_(C++1).cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	double sugarPriceUSD, ricePricePound, sardinesPricePound, coffePriceUSD, milkPriceUSD;
+	float sugarQty, riceQty, sardinesQty, coffeQty, milkQty; 
+	float totalCostPHP, sugarCostPHP, riceCostPHP, sardinesCostPHP, coffeCostPHP, milkCostPHP;
+	double usdToPhp = 50.0, poundToPhp = 72.0;
+	
+	cout << "Enter sugar price USD" << endl;
+	cin >> sugarPriceUSD;
+	cout << "Enter Sugar Quantity" << endl;
+	cin >> sugarQty;
+	cout << "Enter rice Price Pound" << endl;
+	cin >> ricePricePound;
+	cout << "Enter rice Quantity" << endl;
+	cin >> riceQty;
+	cout << "Enter sardines Price Pound" << endl;
+	cin >> sardinesPricePound;
+	cout << "Enter sardines Quantity" << endl;
+	cin >> sardinesQty;
+	cout << "Enter coffe Price USD" << endl;
+	cin >> coffePriceUSD;
+	cout << "Enter coffe Quantity" << endl;
+	cin >> coffeQty;
+	cout << "Enter milk Price USD" << endl;
+	cin >> milkPriceUSD;
+	cout << "Enter milk Quantity" << endl;
+	cin >> milkQty;
+	
+	totalCostPHP = sugarPriceUSD * coffePriceUSD * milkPriceUSD * ricePricePound * sardinesPricePound;
+	totalCostPHP = sugarCostPHP + riceCostPHP + sardinesCostPHP + coffeCostPHP + milkCostPHP;
+	totalCostPHP = (usdToPhp * poundToPhp);
+	
+	return 0;
+}
+    	
+	
